5c9f5efcd97ca0ef584eb0ed749e45de6bf86997
Helena Neves <helenabrn13@gmail.com>
2022-05-05 19:33:36 -0300

    commit de teste

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,8 +3,8 @@
         res.render("admin/form_add_noticia");
     });
     app.post('/noticias/salvar', function (req, res) {
-        //ola     
-        var noticias = req.body;
+        //ola     teste
+        var noticias = req.body; 
         res.send(noticias);
     });
 }
