1e1e41876b6dc2583253d27ed875dc2ef2fdbb38
Helena Neves <helenabrn13@gmail.com>
2022-05-11 19:07:47 -0300

    Exibindo erros de validação e mantendo o preenchimento de formulário

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,6 @@
 ﻿module.exports = function (application) {
     application.get('/formulario_inclusao_noticia', function (req, res) {
-        res.render("admin/form_add_noticia");
+        res.render("admin/form_add_noticia", {validacao: {}, noticia : {}});
     });
     application.post('/noticias/salvar', function (req, res) {
         var noticia = req.body; //noticia é a variável que contém o json que será recuperado do body da nossa request
@@ -10,13 +10,13 @@
         req.assert('resumo', 'Resumo é obrigatório').notEmpty();
         req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10,100);
         req.assert('autor', 'Autor é obrigatório').notEmpty();
-        req.assert('data', 'Data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('data_noticia', 'Data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'});
         req.assert('noticia', 'Noticia é obrigatório').notEmpty();
 
         var errors = req.validationErrors();
 
         if(errors){
-            res.render("admin/form_add_noticia");
+            res.render("admin/form_add_noticia", {validacao : errors, noticia : noticia});
             return;
         }
 
