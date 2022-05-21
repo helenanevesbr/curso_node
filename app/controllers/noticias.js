bd25a98f8a895a34b7937d7923196da2b53e076c
Helena Neves <helenabrn13@gmail.com>
2022-05-20 21:55:28 -0300

    Enviando e recebendo parâmetros (request.query)

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -20,8 +20,10 @@ module.exports.noticia = function(application, req, res){
     var connection = application.config.dbConnection();
     var noticiasModel = new application.app.models.NoticiasDAO(connection);//o primeiro app � para a vari�vel desta fun��o. Se na linha 1 estivesse module.exports = function (application), aqui seria application.app.models.noticiasModel
 
+    var id_noticia = req.query;
+
     //connection.query('select * from noticias where id_noticia = 2', function (error, result) {//consulta seleciona *tudo* de not�cias, mas no apenas na parte onde o id_noticia = 2.
-    noticiasModel.getNoticia(function (error, result) {
+    noticiasModel.getNoticia(id_noticia, function (error, result) {
         if (error) {
             res.send(error);
         } else {
