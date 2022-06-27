57ae3d981f58b33be1b5e91a913304757e275ccc
Helena Neves <helenabrn13@gmail.com>
2022-06-26 22:44:02 -0300

    use express-load

--- config/server.js
+++ config/server.js
@@ -4,6 +4,8 @@
 var express = require('express'); //o require do m�dulo (aka biblioteca) do express retorna uma fun��o, mas n�o chama ela. Agora tem uma fun��o contida na vari�vel express.
 var consign = require('consign'); //recuperando m�dulo
 var bodyParser = require('body-parser');
+var load = require('express-load');
+
 var expressValidator = require('express-validator')
 
 var app = express(); //� necess�rio que chamemos a fun��o.
@@ -20,15 +22,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
 
 
+load({cwd:'app'})
+    .then('app/controllers')
+    .then('app/routes')
+    .then('config/dbConnection.js')
+    .then('app/models')
+    .into(app);
+
+/*
 const consignConfig = process.env.HEROKU  ? { cwd: process.cwd() } : {};
 
-consign(consignConfig)//aqui est� chamando a fun��o criada na linha var consign = require('consign');
-    .include('app/routes')//include tamb�m � uma fun��o. Diret�rio que quer incluir no servidor � um par�metro.
-    .then('config/dbConnection.js')//para n�o ter que digitar um require('../../config/dbConnection') em cada arquivo da pasta rota.
-    //Se voc� n�o apontar a extens�o do arquivo-- .js.O consign entenderia que dbConnection n�o � um m�dulo, mas um subdiret�rio da pasta config.
+consign(consignConfig)
+    .include('app/routes')
+    .then('config/dbConnection.js')
     .then('app/models')
     .then('app/controllers')
-    .into(app); //fun��o include do consign faz o seguinte: executa um scam no diret�rio app/routes, recupera e inicializa os m�dulos dentro dele e atribui isso a vari�vel app. Essa vari�vel vai ser par�metro (ou argumento)? da fun��o export de muitos m�dulos (v�rios arquivos come�am com module.exports = function (app)). Ou seja, todas as rotas, conex�o com banco de dados e models est�o carregados dentro vari�vel app e atrav�z dela os m�dulo tem condi��es de acessar qualquer rota, model e etc. Mas geralmente para de fato acessar, a rota/model/etc � atribu�do a uma vari�vel. Ex: �rota noticia.js ---> var noticiasModel = app.app.models.noticiasModel;
-    //O consign pode fazer auto - load n�o apenas de rotas, mas tamb�m de arquivos de configra��o, de controles, etc...
+    .into(app);
+*/
 
-module.exports = app;//o m�dulo do servidor agora est� exportando o app, que engloba o express, o ejs e todos os m�dulos inclu�dos atrav�z do consign. Espera que na fun��o exportada por cada arquivo de consign (No caso 'app/routes' e 'config/dbConnection.js') que seu primeiro parametro seja "app", mas n�o � necess�rio.
+module.exports = app;
