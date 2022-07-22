4d3d09b53229dfe4655e1d15939573287e6925b8
Helena Neves <helenabrn13@gmail.com>
2022-07-21 22:27:16 -0300

    remove route get formulario_inclusao_noticia until cookie is ready

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,12 +1,4 @@
 ﻿module.exports = function (application) {
-    application.get('/formulario_inclusao_noticia', function (req, res) {
-        if (process.env.ADMIN_ENABLED === 'true'){
-            application.app.controllers.admin.formulario_inclusao_noticia(application, req, res);
-        }
-        else{
-        res.status(404).end('Page is unavailable')
-        }
-    });
     application.post('/noticias/salvar', function (req, res){
         application.app.controllers.admin.noticias_salvar(application, req, res);
     });
